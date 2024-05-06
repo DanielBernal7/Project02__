b5c00721950cb29b5e33bd356f3ad202335c21d1
DanielBernal7 <danbernal@csumb.edu>
2024-05-05 23:03:51 -0700

    Created A JUnit test

--- app/src/test/java/com/example/project02_/AppRepositoryTest.java
+++ app/src/test/java/com/example/project02_/AppRepositoryTest.java
@@ -1,13 +1,21 @@
 package com.example.project02_;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import android.app.Application;
+
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 
+import com.example.project02_.database.AppDatabase;
 import com.example.project02_.database.AppRepository;
+import com.example.project02_.database.ProductDAO;
+import com.example.project02_.database.UserDAO;
 import com.example.project02_.database.entities.Product;
+import com.example.project02_.database.entities.User;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -16,16 +24,26 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 public class AppRepositoryTest {
+    @Mock
+    private UserDAO userDAO;
+
+    @Mock
+    private ProductDAO productDAO;
 
     @Mock
     private AppRepository repository;
 
+    @Mock
+    private Application mockApplication;
+
     @Rule
     public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
+        repository = mock(AppRepository.class);
     }
 
     @Test
@@ -43,5 +61,28 @@ public class AppRepositoryTest {
         repository.deleteProductByName(productName);
         verify(repository).deleteProductByName(productName);
     }
+
+
+    @Test
+    public void testGetUserByUsername() throws Exception {
+        //We create a test user
+        User expectedUser = new User("testUser", "testPassword");
+        when(repository.getUserByUsernameDirectly("testUser")).thenReturn(expectedUser);
+
+
+        User actualUser = repository.getUserByUsernameDirectly("testUser");
+
+        //First we make sure that the user is not null
+        assertNotNull("User should not be null", actualUser);
+        assertEquals("Expected username does not match", "testUser", actualUser.getUsername());
+
+
+        verify(repository).getUserByUsernameDirectly("testUser");
+    }
+
+
+
+
+
 
 }
